7cd26cf834bcc7ebfb084f89272826384f280ead
t0ri <victoriarosemurray@gmail.com>
2019-01-14 17:42:20 -0800

    statistics added

---
+++ public/js/main.js
@@ -0,0 +1,3 @@
+let winsStatistics = document.getElementById('wins-statistics-container')
+let littlesStatistics = document.getElementById('nerves-statistics-container')
+let totalStatistics = document.getElementById('total-statistics-container')
